2c7f3668849766b41ee9a97819439f400b80d7cb
Eduardo Torres Alamo <74425015+EduardoTorresAlamo@users.noreply.github.com>
2023-02-20 16:57:56 -0400

    Create cleanCsv.py

---
+++ cleanCsv.py
@@ -0,0 +1,18 @@
+#
+# EDUARDO TORRES ALAMO
+# 2/20/2023
+#
+
+import pandas as pd
+
+# Read in the .csv file
+# Change 'original.csv' for the desired file to clean
+df = pd.read_csv('original.csv')
+
+# Remove extra spaces and set everything to lowercase
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+df = df.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
+
+# Write the cleaned data to a new .csv file
+# Change 'cleaned.csv' to desired filename.
+df.to_csv('cleaned.csv', index=False)
